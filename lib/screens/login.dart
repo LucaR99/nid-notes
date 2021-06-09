21c36c81326b83ebfe4c5ddd66af1ee1a86444a2
Luca Rossi <lucarossi@MacBook-Pro-di-Luca.local>
2021-06-09 10:54:45 +0200

    inserito drawer

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -32,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
-                      labelText: 'Username pippo'
+                      labelText: 'Username'
                     ),
                     onSaved: (value) {
                       _user.username = value;
